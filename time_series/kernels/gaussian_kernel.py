a70255044551d78563446b16e6b43c9653e1c0bb
James <jameslavis.nz@gmail.com>
2025-10-27 09:47:44 +0100

    added fix for single dimension possibility

--- time_series/kernels/gaussian_kernel.py
+++ time_series/kernels/gaussian_kernel.py
@@ -12,11 +12,11 @@ class GaussianKernel:
         if x1_dim != x2_dim:
             raise Exception("GaussianKernel: x1 and x2 should have the same number of dimensions")
 
-        if x1_dim == 2:
-            ax = 2
+        if x1_dim == 1:
+            return np.exp(-(x2[np.newaxis, :]- x1[:, np.newaxis])**2/(2*self.bandwidth**2))
+        elif x1_dim == 2:
+            return np.exp(-np.linalg.norm(x2[np.newaxis, :, :]- x1[:, np.newaxis, :], axis=2)**2/(2*self.bandwidth**2))
         else:
-            ax = (2, 3)        
+            return np.exp(-np.linalg.norm(x2[np.newaxis, :, :]- x1[:, np.newaxis, :], axis=(2, 3))**2/(2*self.bandwidth**2))
 
-        return np.exp(-np.linalg.norm(x2[np.newaxis, :, :]- x1[:, np.newaxis, :], axis=ax)**2/(2*self.bandwidth**2))
-
-
+        raise Exception("Number of dimensions of the data is too big!")
